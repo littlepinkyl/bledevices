b83a457c38ac281fa4915c24994a568615385168
littlepinkyl <little.pinky@qq.com>
2016-11-03 15:58:28 +0800

    api finished
    post drawables and get drawables with orgId

--- map/views.py
+++ map/views.py
@@ -24,22 +24,40 @@ def get_org(request,org_id=''):
     data =[]
     result={}
 
-    logger.debug(request)
-    showValue=['title','address','floors','longitude','latitude']
-    showValueDict={}
-    for i in showValue:
-        showValueDict[i] = 1
+
+
     if org_id == '':
+        showValue = ['title', 'address', 'floors', 'longitude', 'latitude']
+        showValueDict = {}
+        for i in showValue:
+            showValueDict[i] = 1
         for out in db.organization.find({},showValueDict):
             o_id=out['_id'].__str__()
             out['_id']=o_id
             data.append(out)
     else:
         #Todo,also need to add drawable below
-        for out in db.organization.find({'_id':ObjectId(org_id)},showValueDict):
-            o_id=out['_id'].__str__()
-            out['_id']=o_id
-            data.append(out)
+        showValuePart = ['title', 'floor', 'floors']
+        showValuePartDict = {}
+        for i in showValuePart:
+            showValuePartDict[i] = 1
+        logger.debug(org_id)
+        for outComePart in db.part.find({'owner':ObjectId(org_id)},showValuePartDict):
+            #logger.debug(outComePart)
+            outComePart['drawables']=[]
+            showValueDrawable=['title','type','longitude','latitude','data']
+            showValueDrawableDict={}
+            for i in showValueDrawable:
+                showValueDrawableDict[i]=1
+            for outComeDrawble in db.drawable.find({'part':ObjectId(outComePart['_id'])},showValueDrawableDict):
+                o_id=outComeDrawble['_id'].__str__()
+                outComeDrawble['_id']=o_id
+                outComePart['drawables'].append(outComeDrawble)
+
+            o_id=outComePart['_id'].__str__()
+            outComePart['_id']=o_id
+            data.append(outComePart)
+            logger.debug(data)
 
     result['status']='ok'
     result['data']=data
@@ -49,32 +67,38 @@ def get_org(request,org_id=''):
 @csrf_exempt
 def post_drawing(request,part_id):
     body={}
-    try:
+    re={}
+    re['obj_id']=[]
+    try:#
         body = json.loads(request.body)
 
     except Exception,e:
-        logger.debug('---{0}---{1}---{2}'.format(Exception,e,request.body))
+        logger.debug('[1]---{0}---{1}---{2}'.format(Exception,e,request.body))
 
     drawing=db.drawable
     now=datetime.datetime.now()
     current={}
-    try:
-        for i in body['drawing']:
+    pre_id=''
+
+    for i in body['drawing']:
+        try:
             current=i
+
             current['create_on']=now
             current['update_on']=now
             current['part']=ObjectId(body['part'])
-            #write to db
-        pre_id = drawing.insert_one(current).inserted_id
-    except Exception,e:
-        result={}
-        result['status']='Failed'
-        current['part']=body['part']
-        result['data']=current['data']
-        logger.debug('----{0}---{1}---{2}'.format(Exception,e,body))
-        return HttpResponse(json.dumps(result,encoding='utf8',indent=4,content_type="application/json"))
+            pre_id = drawing.insert_one(current).inserted_id
 
-    body['status']='ok'
-    body['part_id']=part_id
+            if isinstance(pre_id, ObjectId):
+                re['obj_id'].append({'pre_id': pre_id.__str__(), "title": current['title']})
+            else:
+                #
+                logger.debug('[ERROR]-----cannot insert :{0}---{1}'.format(current['title'],current))
+                re['ERROR'].append(current['title'])
+        except Exception,e:
+            re['obj_id'].append({'pre_id': pre_id.__str__(), "title": current['title'],"exception":'[{0}]'.format(e)})
 
-    return HttpResponse(json.dumps(body, encoding='utf8', indent=4), content_type="application/json")
+    re['status']='ok'
+    re['part_id']=part_id
+
+    return HttpResponse(json.dumps(re, encoding='utf8', indent=4), content_type="application/json")
